4e1b4cb3394af7d6d2231bb7e6f4742e56d2c0e2
biyakim <w2116@e-mirim.hs.kr>
2023-03-16 14:04:07 +0900

    object

--- 문법.js
+++ 문법.js
@@ -27,9 +27,33 @@
 // result = sub(3,2);
 // console.log(result) //1
 
-square = num => num*num;
+// square = num => num*num;
 
-result = square(3);
-console.log(result) //9
+// result = square(3);
+// console.log(result) //9
+
+
+
+// function varTest() {
+//   var v = 1;
+//   if(true) {
+//     var v = 2; //상위 블록과 같은 변수
+//     console.log(v); //2
+//   }
+//   console.log(v); //2
+// }
+// function letTest() {
+//   let l = 1;
+//   if(true) {
+//     let l = 2; //상위 블록과 다른 변수
+//     console.log(l); //2
+//   }
+//   console.log(l); //1
+// }
+
+
+// varTest();
+// letTest();
+
 
 
