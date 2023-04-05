9e43fb8f3f52255801006133f122c3cf0a4ab86f
biyakim <w2116@e-mirim.hs.kr>
2023-04-05 09:54:19 +0900

    fs.statSync

--- 문법/fs.js
+++ 문법/fs.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 console.log(__dirname);
 
 // 해당 경로에 있는 파일 목록의 배열을 반환
-const files = fs.readdir(__dirname);
+const files = fs.readdirSync(__dirname);
 
-console.log(files);
+//특정파일의 전체(절대)경로
+result = ___dirname + '\\'+files[1]
+console.log(result)
+
+console.log(fs.statSync(result));
