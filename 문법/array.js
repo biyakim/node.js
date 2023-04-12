2b1d7a64e6fc2f15a4f4d85bbae8a2a8757b0a16
biyakim <w2116@e-mirim.hs.kr>
2023-04-12 10:02:10 +0900

    array.findIndex()

--- 문법/array.js
+++ 문법/array.js
@@ -2,5 +2,12 @@ const arr = ['apple','banana', 'cherry', 'apple'];
 
 //data: 데이터, index: 데이터에 해당하는 index(생략가능)
 arr.forEach(function(data, index) {
-  console.log(`${index} : ${data}`);
+  //console.log(`${index} : ${data}`);
 })
+
+//return 값이 true에 해당하는 index 반환,
+const cherryIndex = arr.findIndex(function(data) {
+  return data === 'cherry';
+})
+
+console.log(cherryIndex)
