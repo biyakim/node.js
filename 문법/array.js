c6e641a1c53211dc8cb76a0b46d2fd45053ba468
biyakim <w2116@e-mirim.hs.kr>
2023-04-12 10:17:15 +0900

    array.indexOf()

--- 문법/array.js
+++ 문법/array.js
@@ -9,6 +9,9 @@ arr.forEach(function(data, index) {
 const cherryIndex = arr.findIndex(data => data === 'cherry');
 //console.log(cherryIndex)
 
+//3번 index부터 'cherry'에 해당하는 index를 찾음, 없으면 -1
+const a = arr.indexOf('cherry', 0);
+console.log(a)
 
 arr.push('grape');
 console.log(arr);
