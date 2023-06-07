c6b2ac49ebff59200ba8570552e508d33c468766
biyakim <w2116@e-mirim.hs.kr>
2023-06-07 10:00:41 +0900

    .env로 시스템정보 감추기

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql2');
+const path = require('path');
+
+require('dotenv').config({
+  path : path.join(__dirname, '.env')
+});
 
 const pool = mysql.createPool({
-  host : 'localhost',
-  user : 'root',
-  password : '1234',
-  database : 'lck',
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DB,
 });
 
 pool.query("SELECT * FROM player",(err,results) => {
