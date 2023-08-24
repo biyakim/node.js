06e7425a4e3d165c498f19c92d25da2ffdcd5ea6
biyakim <w2116@e-mirim.hs.kr>
2023-08-24 13:37:29 +0900

    로그인 회원가입

--- session/app.js
+++ session/app.js
@@ -36,4 +36,24 @@ app.get("/",(req,res) => {
         }
 });
 
+app.post("/process/login", (req,res) => {
+    const {password, username} = req.body
+
+    if(req.session.user) {
+        console.log("이미 로그인중");
+        return res.render(path.join(__dirname,"views","loging")
+            ,{username : req.session.user.id});
+    }
+    else{
+        req.session.user = {
+            id : username,
+            pw : password,
+            authorized :  true
+        };
+        return res.render(path.join(__dirname, "views", "logined"), {
+            username : username
+        })
+    }
+})
+
 app.listen(3000);
